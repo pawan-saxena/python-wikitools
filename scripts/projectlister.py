21ee42c5de4d3989c6a16f7d50564ab48f0af72b
MrZmanwiki <MrZmanwiki@52ee0a72-91a9-11dd-ab33-2764402e1b4d>
2009-01-11 01:19:31 +0000

    updates

--- scripts/projectlister.py
+++ scripts/projectlister.py
@@ -1,2 +1,10 @@
-def getList():
-	return {'dino': ['dinosaurs', 'dino', 'Wikipedia:WikiProject Dinosaurs/Popular pages'], 'guns': ['Firearms', 'guns', 'Wikipedia:WikiProject Firearms/Popular pages'], 'nrhp': ['National Register of Historic Places', 'nrhp', 'Wikipedia:WikiProject National Register of Historic Places/Popular pages'], 'av': ['aviation', 'av', 'Wikipedia:WikiProject Aviation/Popular pages'], 'utsh': ['Utah road transport', 'utsh', 'Wikipedia:WikiProject Utah State Highways/Popular pages'], 'wpmi': ['Michigan', 'wpmi', 'Wikipedia:WikiProject Michigan/Popular pages'], 'pal': ['Palaeontology', 'pal', 'Wikipedia:WikiProject Palaeontology/Popular pages'], 'bird': ['bird', 'bird', 'Wikipedia:WikiProject Birds/Popular pages']}
+projects = {'dino': ['dinosaurs', 'Wikipedia:WikiProject Dinosaurs/Popular pages'],
+	'bird': ['bird', 'Wikipedia:WikiProject Birds/Popular pages'],
+	'nrhp': ['National Register of Historic Places', 'Wikipedia:WikiProject National Register of Historic Places/Popular pages'],
+	'av': ['aviation', 'Wikipedia:WikiProject Aviation/Popular pages'],
+	'ohio': ['Ohio', 'Wikipedia:WikiProject Ohio/Popular pages'],
+	'utsh': ['Utah road transport', 'Wikipedia:WikiProject Utah State Highways/Popular pages'],
+	'wpmi': ['Michigan', 'Wikipedia:WikiProject Michigan/Popular pages'],
+	'pal': ['Palaeontology', 'Wikipedia:WikiProject Palaeontology/Popular pages'],
+	'guns': ['Firearms', 'Wikipedia:WikiProject Firearms/Popular pages'],
+}
